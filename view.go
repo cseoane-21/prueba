d7e7290eeb08144520321be94fb90c39a9657fde
Camila Seoane <camila.seoane@mercadolibre.com>
2024-01-19 13:41:13 -0300

    add y remove

--- view.go
+++ view.go
@@ -1,6 +1,8 @@
 package prueba
 
 async function controller2(req, res) {
+	const prueba = "prueba"
 const conn = await getConnection()
 conn.query("SELECT * FROM Post WHERE id = ?", req.params.id);
+// comentario
 }
